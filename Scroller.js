476a70394dcaa786416843ee0f9b903f51c20b45
antipda <antipda@ite-ng.ru>
2024-08-26 14:26:26 +0300

    prototype единый для всех abstractScroller'ов - поэтому нельзя в нем устраивать хранилище параметров. ОН может реализовывать лишь функционал.

--- Scroller.js
+++ Scroller.js
@@ -203,44 +203,7 @@
 
 
     function abstractScroller (){
-        this.id = undefined;
-        /**
-         * Callback'и, навешанные на систему. При удалении экземпляра компонента эти callback'и следует также удалять.
-         */
-        this.callbacks = {};
-        this.config = {}; //Параметры экземпляра Scroller'а
-        /**
-         * Высота виртуального элемента
-         * @type {number}
-         */
-        this.virtualElementSize = 40;//??remove
-        this.scrollSensitivity = 40;
-        this.scrollForwardEnough = true; //Флаг, указывающий, что при очередной прокрутке вперед элементов было достаточно для заполнения области просмотра
-        this.currentScroll = 0; //Величина текущей прокрутки обертки //??remove
-        this.scrollDirection = 0; // Направление прокрутки 1, -1 //??remove
-        this.styleContainer = undefined;
-        this.styles = {//TODO Теоретически можно использовать единый объект style для подобных стилей, которые не обязательно должны быть связаны с конкретным экземпляром
-            '.root-scrolled-container': 'display: flex; flex-direction: column;',
-            '.scroller-wrapper': 'display: flex; margin: 0px; padding: 0px; border: 0px none; flex-grow: 1; flex-wrap: nowrap; flex-direction: row; overflow: hidden;',
-            '.scroller-data-container': 'display: flex; /*flex-direction: column;*/ flex-wrap: nowrap; overflow-y: hidden; overflow-x: scroll;',
-            '.scroller-scrollbar-container': 'overflow-x: hidden; overflow-y: scroll; min-width: 18px; max-width: 18px; width: 18px;',
-            '.scroller-scrollbar': 'display: inline-block; min-width: 1px; width: 1px; max-width: 1px; opacity: 0;',
-            '.transparent': 'opacity: 0;',
-            '.mode-scroll .scroller-data-container .scrolled-item': 'flex-grow: 0 !important; flex-shrink: 0 !important;',
-            '.scroller-data-container .scrolled-item:last-child': 'flex-shrink: 1;'
-        };
 
-        this.items = {
-            items: {}, //Загруженные в настоящий момент элементы по индексам - пока не используется. Возможно, может обновляться асинхронно, но функции обновления должны работать в порядке очередности
-        };
-        this.DOM = {
-            wrapper: undefined,
-            dataContainer: undefined,
-            scrollbarContainer: undefined,
-            scrollbar: undefined,
-            scrolledContainer: undefined,
-            scrolledItemsContainer: undefined,
-        };
         this.init = function(){
             this.createId();
             this.createStyleElement();
@@ -622,6 +585,44 @@
 
             standardScroller.prototype.wrap.call(this);
 
+        };
+
+        this.id = undefined;
+        /**
+         * Callback'и, навешанные на систему. При удалении экземпляра компонента эти callback'и следует также удалять.
+         */
+        this.callbacks = {};
+        /**
+         * Высота виртуального элемента
+         * @type {number}
+         */
+        this.virtualElementSize = 40;//??remove
+        this.scrollSensitivity = 40;
+        this.scrollForwardEnough = true; //Флаг, указывающий, что при очередной прокрутке вперед элементов было достаточно для заполнения области просмотра
+        this.currentScroll = 0; //Величина текущей прокрутки обертки //??remove
+        this.scrollDirection = 0; // Направление прокрутки 1, -1 //??remove
+        this.styleContainer = undefined;
+        this.styles = {//TODO Теоретически можно использовать единый объект style для подобных стилей, которые не обязательно должны быть связаны с конкретным экземпляром
+            '.root-scrolled-container': 'display: flex; flex-direction: column;',
+            '.scroller-wrapper': 'display: flex; margin: 0px; padding: 0px; border: 0px none; flex-grow: 1; flex-wrap: nowrap; flex-direction: row; overflow: hidden;',
+            '.scroller-data-container': 'display: flex; /*flex-direction: column;*/ flex-wrap: nowrap; overflow-y: hidden; overflow-x: scroll;',
+            '.scroller-scrollbar-container': 'overflow-x: hidden; overflow-y: scroll; min-width: 18px; max-width: 18px; width: 18px;',
+            '.scroller-scrollbar': 'display: inline-block; min-width: 1px; width: 1px; max-width: 1px; opacity: 0;',
+            '.transparent': 'opacity: 0;',
+            '.mode-scroll .scroller-data-container .scrolled-item': 'flex-grow: 0 !important; flex-shrink: 0 !important;',
+            '.scroller-data-container .scrolled-item:last-child': 'flex-shrink: 1;'
+        };
+
+        this.items = {
+            items: {}, //Загруженные в настоящий момент элементы по индексам - пока не используется. Возможно, может обновляться асинхронно, но функции обновления должны работать в порядке очередности
+        };
+        this.DOM = {
+            wrapper: undefined,
+            dataContainer: undefined,
+            scrollbarContainer: undefined,
+            scrollbar: undefined,
+            scrolledContainer: undefined,
+            scrolledItemsContainer: undefined,
         };
 
     };

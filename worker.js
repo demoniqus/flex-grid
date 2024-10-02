d0123fea0458f392a510479724a723e33d55f589
antipda <antipda@ite-ng.ru>
2024-10-02 09:28:18 +0300

    Воркеры, как и общение между окнами, не позволяет передавать методы, ссылки и т.п. Они все, похоже, просто работают на основе сериализации и десериализации данных. Т.е. фактически после передачи данных это уже получаются к тому же совсем другие экземпляры объектов, не тождественные исходным. А потому все воркеры бесполезны для грида.

--- worker.js
+++
@@ -1,8 +0,0 @@
-onmessage = function(e){
-    let counter = 0;
-    let i = setInterval(function(){
-        postMessage({count: ++counter})
-        if (counter > 100) {
-            clearInterval(i)};
-    }, 1000);
-};

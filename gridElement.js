2dcc55f524ec0b7824a0fa77fe9494a1e8b159de
antipda <antipda@ite-ng.ru>
2024-10-02 09:23:42 +0300

    refactoring

--- gridElement.js
+++ gridElement.js
@@ -27,102 +27,107 @@ export function GridElement(config, privFlexGrid, pubFlexGrid){
         pubFlexGrid: pubFlexGrid,
     };
 
-    let pub = {
-        initData: function(/**@type {object}*/data){
-            this.data = data;
-        }.bind(priv),
-        setData: function(/**@type {string}*/key, data){
-            this.data[key] = data;
-        }.bind(priv),
-        get: function(/**@type {string}*/key) {
-            return key in this.data ? this.data[key]: undefined;
-        }.bind(priv),
-        getData: function(){return this.data;}.bind(priv),
-        expand: function(/**@type {DataSetInterface} */dataSet, /**@type {boolean}*/ expanded){
-            !dataSet && (dataSet = priv.privFlexGrid.data.current);
-            dataSet.expand(this, !!expanded);
-        },
-        expanded: function(/**@type {DataSetInterface} */dataSet){
-            !dataSet && (dataSet = priv.privFlexGrid.data.current);
-            return dataSet.expanded(this);
-        },
-        inverseExpanded: function(/**@type {DataSetInterface} */dataSet){
-            !dataSet && (dataSet = priv.privFlexGrid.data.current);
-            return dataSet.inverseExpanded(this);
-        },
-        setParent: function(/** @type {GridElement} */parent){
-            if (this.parent && this.parent !== parent) {
-                //Удаляем из коллекции дочерних элементов старого родителя
-                this.parent.removeChild(this);
-            }
+    this.initData = function(/**@type {object}*/data){
+        priv.data = data;
+    };
 
-            this.parent = parent;
+    this.setData = function(/**@type {string}*/key, data){
+        priv.data[key] = data;
+    };
 
-            if (this.parent) {
-                //Добавляем в коллекцию дочерних элементов к новому родителю
-                parent.addChild(pub);
-            }
-        }.bind(priv),
-        removeChild: function(/** @type {GridElement} */child){
-            let iof = priv.children.indexOf(child);
-            if (iof > -1) {
-                priv.children.splice(iof, 1)
-            }
-            // let c = priv.children.length;
-            // let res = [];
-            // for (let i = 0; i < c; i++) {
-            //     if (priv.children[i] !== child) {
-            //         res.push(priv.children[i]);
-            //     }
-            // }
-            // priv.children = res;
-        }.bind(priv),
-        addChild: function(/** @type {GridElement} */child){
-            if (!priv.children.includes(child)) {
-                priv.children.push(child);
-            }
-        }.bind(priv),
+    this.get = function(/**@type {string}*/key) {
+        return key in priv.data ? priv.data[key]: undefined;
+    };
 
-        updateCell: function(propName){
-            let header = this.privFlexGrid.headers.dict[propName];
-            //аргументы могут быть использованы при пользовательской функции, определяющей конкретный visualizer
-            let visualizer = header.getVisualizer(propName, item, header, privFlexGrid.config);
+    this.getData = function(){return priv.data;};
 
-            visualizer && visualizer.buildReadForm(
-                this.DOM.cells[headerId],
-                headerId,
-                this,
-                header,
-                //index
-            );
-        }.bind(priv),
+    this.expand = function(/**@type {DataSetInterface} */dataSet, /**@type {boolean}*/ expanded){
+        !dataSet && (dataSet = priv.privFlexGrid.data.current);
+        dataSet.expand(this, !!expanded);
+    };
 
-        getGrid: function(){
-            return this.pubFlexGrid
-        }.bind(priv)
+    this.expanded = function(/**@type {DataSetInterface} */dataSet){
+        !dataSet && (dataSet = priv.privFlexGrid.data.current);
+        return dataSet.expanded(this);
+    };
+
+    this.inverseExpanded = function(/**@type {DataSetInterface} */dataSet){
+        !dataSet && (dataSet = priv.privFlexGrid.data.current);
+        return dataSet.inverseExpanded(this);
+    };
+
+    this.setParent = function(/** @type {GridElement} */parent){
+        if (priv.parent && priv.parent !== parent) {
+            //Удаляем из коллекции дочерних элементов старого родителя
+            priv.parent.removeChild(priv);
+        }
+
+        priv.parent = parent;
+
+        if (priv.parent) {
+            //Добавляем в коллекцию дочерних элементов к новому родителю
+            parent.addChild(this);
+        }
+    };
+
+    this.removeChild = function(/** @type {GridElement} */child){
+        let iof = priv.children.indexOf(child);
+        if (iof > -1) {
+            priv.children.splice(iof, 1)
+        }
+        // let c = priv.children.length;
+        // let res = [];
+        // for (let i = 0; i < c; i++) {
+        //     if (priv.children[i] !== child) {
+        //         res.push(priv.children[i]);
+        //     }
+        // }
+        // priv.children = res;
+    };
+
+    this.addChild = function(/** @type {GridElement} */child){
+        if (!priv.children.includes(child)) {
+            priv.children.push(child);
+        }
+    };
+
+    this.updateCell = function(propName){
+        let header = priv.privFlexGrid.headers.dict[propName];
+        //аргументы могут быть использованы при пользовательской функции, определяющей конкретный visualizer
+        let visualizer = header.getVisualizer(propName, item, header, privFlexGrid.config);
+
+        visualizer && visualizer.buildReadForm(
+            priv.DOM.cells[headerId],
+            headerId,
+            priv,
+            header,
+            //index
+        );
+    };
+
+    this.getGrid = function(){
+        return priv.pubFlexGrid
     };
 
     Object.defineProperties(
-        pub,
+        this,
         {
             parent: {
                 configurable: false,
                 enumerable: false,
-                get: function(){return this.parent;}.bind(priv),
+                get: function(){return priv.parent;},
             },
             children: {
                 configurable: false,
                 enumerable: false,
-                get: function(){return this.children;}.bind(priv),
+                get: function(){return priv.children;},
             },
             DOM: {
                 configurable: false,
                 enumerable: false,
-                get: function(){return this.DOM;}.bind(priv),
+                get: function(){return priv.DOM;},
             },
         }
 
     );
-
-    return pub;
 }

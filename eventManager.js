cf4c6a836f4d2cbd16999222329ba0ae673a623c
antipda <antipda@ite-ng.ru>
2024-10-30 07:18:08 +0300

    tab style for Netbeans

--- eventManager.js
+++ eventManager.js
@@ -67,101 +67,101 @@ function Event(
 	/**@type {string} наименование события */ eventName,
 	/**@type {Object|null} конфигурация события */ config
 ){
-	let priv = {
-		sourceObject: object,
-		eventName: eventName,
-		/**@type WeakMap <CallableFunction, Object> Словарь подписчиков на события */
-		dict: new WeakMap(),
-		//Список подписчиков на события
-		list: [],
-		returnResult: config && 'returnResult' in config ? !!config.returnResult : false,
-	};
+    let priv = {
+            sourceObject: object,
+            eventName: eventName,
+            /**@type WeakMap <CallableFunction, Object> Словарь подписчиков на события */
+            dict: new WeakMap(),
+            //Список подписчиков на события
+            list: [],
+            returnResult: config && 'returnResult' in config ? !!config.returnResult : false,
+    };
 
-	let t;
+    let t;
 
-	let sync = function(){
-		//Периодически выполняем проверку, все ли подписчики еще существуют
-		clearTimeout(t);
-		let i = 0;
+    let sync = function(){
+            //Периодически выполняем проверку, все ли подписчики еще существуют
+            clearTimeout(t);
+            let i = 0;
 
-		while (i < priv.list.length) {
-			let subscriber = priv.list[i];
-			if (!priv.dict.has(subscriber.callback)) {
-				priv.list[i] = null;
-			}
-			i++;
-		}
-		priv.list = priv.list.filter((subscriber) => !!subscriber);
-	};
+            while (i < priv.list.length) {
+                    let subscriber = priv.list[i];
+                    if (!priv.dict.has(subscriber.callback)) {
+                            priv.list[i] = null;
+                    }
+                    i++;
+            }
+            priv.list = priv.list.filter((subscriber) => !!subscriber);
+    };
 
-	this.fire = priv.returnResult ?
-		function(
-			/**@type {Object|null} параметры конкретного экземпляра события*/ baseEventParams
-		){
-			let i = -1;
+    this.fire = priv.returnResult ?
+            function(
+                    /**@type {Object|null} параметры конкретного экземпляра события*/ baseEventParams
+            ){
+                    let i = -1;
 
-			let eventParams = {
-				_sourceEventParams: 'Параметры текущего экземпляра события',
-				sourceEventParams: baseEventParams,
-				_sourceObject: 'Объект-источник события',
-				sourceObject: priv.sourceObject,
-				_eventName: 'Наименование события',
-				eventName: priv.eventName,
-				_eventExtendedParams: 'Стандартные параметры подписчика на событие'
-			};
-			// Количество подписчиков на событие может быть любым. Поэтому результат реакции на любое событие всегда представляет из себя массив
-			let res = [];
+                    let eventParams = {
+                            _sourceEventParams: 'Параметры текущего экземпляра события',
+                            sourceEventParams: baseEventParams,
+                            _sourceObject: 'Объект-источник события',
+                            sourceObject: priv.sourceObject,
+                            _eventName: 'Наименование события',
+                            eventName: priv.eventName,
+                            _eventExtendedParams: 'Стандартные параметры подписчика на событие'
+                    };
+                    // Количество подписчиков на событие может быть любым. Поэтому результат реакции на любое событие всегда представляет из себя массив
+                    let res = [];
 
-			while (++i < priv.list.length) {
-				let subscriber = priv.list[i];
-				if (!priv.dict.has(subscriber.callback)) {
-					continue;
-				}
-				eventParams.eventExtendedParams = subscriber.eventExtendedParams;
+                    while (++i < priv.list.length) {
+                            let subscriber = priv.list[i];
+                            if (!priv.dict.has(subscriber.callback)) {
+                                    continue;
+                            }
+                            eventParams.eventExtendedParams = subscriber.eventExtendedParams;
 
-				res.push(subscriber.callback(eventParams));
-			}
+                            res.push(subscriber.callback(eventParams));
+                    }
 
-			clearTimeout(t);
-			t = setTimeout(sync, 50);
+                    clearTimeout(t);
+                    t = setTimeout(sync, 50);
 
-			return res;
-		} :
-		function(
-			/**@type {Object|null} параметры конкретного экземпляра события*/ baseEventParams
-		){
-			let i = -1;
+                    return res;
+            } :
+            function(
+                    /**@type {Object|null} параметры конкретного экземпляра события*/ baseEventParams
+            ){
+                    let i = -1;
 
-			let eventParams = {
-				_sourceEventParams: 'Параметры текущего экземпляра события',
-				sourceEventParams: baseEventParams,
-				_sourceObject: 'Объект-источник события',
-				sourceObject: priv.sourceObject,
-				_eventName: 'Наименование события',
-				eventName: priv.eventName,
-				_eventExtendedParams: 'Стандартные параметры подписчика на событие'
-			};
+                    let eventParams = {
+                            _sourceEventParams: 'Параметры текущего экземпляра события',
+                            sourceEventParams: baseEventParams,
+                            _sourceObject: 'Объект-источник события',
+                            sourceObject: priv.sourceObject,
+                            _eventName: 'Наименование события',
+                            eventName: priv.eventName,
+                            _eventExtendedParams: 'Стандартные параметры подписчика на событие'
+                    };
 
-			while (++i < priv.list.length) {
-				let subscriber = priv.list[i];
-				if (!priv.dict.has(subscriber.callback)) {
-					continue;
-				}
-				eventParams.eventExtendedParams = subscriber.eventExtendedParams;
+                    while (++i < priv.list.length) {
+                            let subscriber = priv.list[i];
+                            if (!priv.dict.has(subscriber.callback)) {
+                                    continue;
+                            }
+                            eventParams.eventExtendedParams = subscriber.eventExtendedParams;
 
-				subscriber.callback(eventParams);
-			}
+                            subscriber.callback(eventParams);
+                    }
 
-			clearTimeout(t);
-			t = setTimeout(sync, 50);
-		};
+                    clearTimeout(t);
+                    t = setTimeout(sync, 50);
+            };
 
-	this.subscribe = function(callback, eventExtendedParams)
-	{
-		let subscriber = {callback, eventExtendedParams};
-		if (!priv.dict.has(callback)) {
-			priv.dict.set(callback, subscriber);
-			priv.list.push(subscriber);
-		}
-	}
+    this.subscribe = function(callback, eventExtendedParams)
+    {
+            let subscriber = {callback, eventExtendedParams};
+            if (!priv.dict.has(callback)) {
+                    priv.dict.set(callback, subscriber);
+                    priv.list.push(subscriber);
+            }
+    }
 }
